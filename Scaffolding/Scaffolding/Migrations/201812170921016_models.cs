aec00277ebc86be01b10f0d98a401341d5a1b9fe
imran <i.mran.javed62@hotmail.com>
2018-12-17 09:26:47 +0000

    EF

---
+++ Scaffolding/Scaffolding/Migrations/201812170921016_models.cs
@@ -0,0 +1,16 @@
+namespace Scaffolding.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class models : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
